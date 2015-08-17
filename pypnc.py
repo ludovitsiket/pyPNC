197bb0dbdce1a00934e0f7504a900dc0d31cafd8
ludovitsiket <siket.ludovit@gmail.com>
2015-08-17 14:49:36 +0200

    formalne upravy

--- pypnc.py
+++ pypnc.py
@@ -6,15 +6,12 @@ import zipfile
 def chyba():
     print "Nezadal si vsetky potrebne parametre programu."
     print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru"
-    print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru = nazov pod ktorym sa komprimovany subor ulozi \n"
+    print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru.zip = nazov pod ktorym sa komprimovany subor ulozi. Za meno suboru treba dopisat .zip \n"
 
 def prg ():
     odkial=sys.argv[1]
     kam=sys.argv[2]
     meno=sys.argv[3]
-    print "odkial ",odkial
-    print "kam ",kam
-    print "meno suboru ",meno
     source=odkial
     zip=zipfile.ZipFile(meno, 'w',zipfile.ZIP_DEFLATED)
     rootlen=len(source)+1
@@ -22,8 +19,8 @@ def prg ():
         for file in files:
             fn=os.path.join(base,file)
             zip.write(fn,fn[rootlen:])
+    print "[OK]\nSubor sa nachadza v urcenom adresary",kam
 
 if len(sys.argv)<=3: #program vyzaduje 3 parametre
     chyba()
 else: prg()
-#print sys.argv[1] #prvy parameter po nazve programu
