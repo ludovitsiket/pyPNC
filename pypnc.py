46905168d2ae1fb2577ed83683d6d490e2cc170f
ludovitsiket <siket.ludovit@gmail.com>
2015-08-17 15:17:50 +0200

    Pridanie casu a pripony do suboru po zadani jeho nazvu

--- pypnc.py
+++ pypnc.py
@@ -3,17 +3,21 @@
 import sys
 import os
 import zipfile
+from time import gmtime, strftime
+
 def chyba():
     print "Nezadal si vsetky potrebne parametre programu."
     print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru"
-    print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru.zip = nazov pod ktorym sa komprimovany subor ulozi. Za meno suboru treba dopisat .zip \n"
+    print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru.zip = nazov pod ktorym sa komprimovany subor ulozi. Pripona .zip bude pridana automaticky.\n"
 
 def prg ():
     odkial=sys.argv[1]
     kam=sys.argv[2]
     meno=sys.argv[3]
     source=odkial
-    zip=zipfile.ZipFile(meno, 'w',zipfile.ZIP_DEFLATED)
+    cas=strftime("_%Y-%m-%d_%H:%M:%S", gmtime())
+    subor=meno+cas+".zip"
+    zip=zipfile.ZipFile(subor, 'w',zipfile.ZIP_DEFLATED)
     rootlen=len(source)+1
     for base,dirs,files in os.walk(source):
         for file in files:
