39e39d0e82b6d9b8def292aeaa2e1fed5b811e55
ludovitsiket <siket.ludovit@gmail.com>
2015-08-21 12:33:36 +0200

    pokracovanie ftp serveru, vyrieseny login

--- pypnc.py
+++ pypnc.py
@@ -4,20 +4,35 @@ import sys
 import os
 import zipfile
 from time import gmtime, strftime
-#from ftplib import FTP  #kvoli pripojeniu na FTP server
-import ftplib
+from ftplib import FTP
+meno=[]
+ps=[]
+my_list=[]
 
 def chyba():
     print "Nezadal si vsetky potrebne parametre programu."
     print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru"
     print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru.zip = nazov pod ktorym sa komprimovany subor ulozi. Pripona .zip bude pridana automaticky.\n"
 
+def citaj (): 
+    with open('/home/peter/pwd','r') as infile:
+        data=infile.read()
+    my_list=data.splitlines()
+    meno=my_list[0]
+    ps=my_list[1]
+    
 def connect ():
-    ftp=ftplib.FTP('localhost', 'anonymous', 'anonymous@localhost')
-    print "Zoznam suborov"
+    citaj ()
+    ftp=FTP('localhost') #tu je adresa serveru ftp
+    ftp.login(meno,ps)
+    ftp.getwelcome() #uvitacia sprava zo serveru
+    aaa=ftp.retrlines('LIST')
+    print aaa
+    adr=ftp.pwd()
+    print "Aktualny adresar:",adr
     files=ftp.dir()
-    print files 
-    ftp.cwd("/")
+    print "Obsah adresara:",files 
+    print "Koniec spojenia.",ftp.quit()
 
 def prg ():
     print "Cakajte prosim, program robi co moze."
@@ -39,3 +54,4 @@ if len(sys.argv)<=3: #program vyzaduje 3 parametre
     chyba()
 #else: prg()
 else: connect ()
+
