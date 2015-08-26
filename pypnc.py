7259024b466b2e22f62a99cf03bcbf43ce5bc25a
ludovitsiket <siket.ludovit@gmail.com>
2015-08-26 17:59:01 +0200

    funkcne, zipuje, pripaja sa a aj subory prenasa

--- pypnc.py
+++ pypnc.py
@@ -5,9 +5,11 @@ import os
 import zipfile
 from time import gmtime, strftime
 from ftplib import FTP_TLS 
+import fnmatch
 meno=[]
 ps=[]
 my_list=[]
+souborek=""
 def chyba():
     print "Nezadal si vsetky potrebne parametre programu."
     print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru"
@@ -16,32 +18,37 @@ def chyba():
 if len(sys.argv)<=3: #program vyzaduje 3 parametre
    chyba()
 else:
-    print "Cakajte prosim, program robi co moze. Naozaj sa snazi."
+    print "Cakajte prosim, program robi co moze." 
     odkial=sys.argv[1]
     kam=sys.argv[2]
     meno=sys.argv[3]
     source=odkial
     cas=strftime("_%Y-%m-%d_%H:%M:%S", gmtime())
-    subor=meno+cas+".zip"
+    subor=meno+cas+".zip" #vysledne meno suboru
     zip=zipfile.ZipFile(subor, 'w',zipfile.ZIP_DEFLATED) # nezipuje velke subory (treba zip64 extension)!
     rootlen=len(source)+1
     for base,dirs,files in os.walk(source):
         for file in files:
             fn=os.path.join(base,file)
-            souborek=zip.write(fn,fn[rootlen:])
-            suborik=str(souborek)
+            souborek=zip.write(fn,fn[rootlen:]) #souborek je hotovy zazipovany subor
+            suborik=str(souborek) #string premennej souborek
     print "[OK]\nSubor sa nachadza v urcenom adresary",kam
     with open('/home/peter/pwd','r') as infile:
         data=infile.read()
     my_list=data.splitlines()
-    meno=my_list[0]
+    meno2=my_list[0]
     ps=my_list[1]
     path=my_list[2]
     server=my_list[3]
-    ftp=FTP_TLS(server,meno,ps) 
+    for file in os.listdir('.'):
+        if fnmatch.fnmatch(file,"*.zip"):
+            fajl=str(file) #tymto tu si do fajl ulozim meno hotoveho suboru na odoslanie; pouzita kniznica fnmatch
+    ftp=FTP_TLS(server,meno2,ps)   #pripojenie k serveru atd. 
     ftp.prot_p()
-    ftp.cwd(my_list[2])
+    ftp.cwd(my_list[2]) #prepnutie do prislusneho adresara ftps serveru
     ftp.retrlines("LIST")
-    print "Posielam subor. Znova cakajte..."
-    ftp.sendcmd("STOR suborik")
+    print "Posielam subor. Cakajte..."
+    ftp.storbinary('STOR '+fajl, open(fajl, 'rb'))
     ftp.close()
+    print "[OK]\n"
+    print "Spojenie ukoncene.\n"
