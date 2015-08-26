651322f16c580448ab0fcb345be6407776f88cba
ludovitsiket <siket.ludovit@gmail.com>
2015-08-26 12:03:39 +0200

    program najskor kontroluje ci ma potrebne parametre

--- pypnc.py
+++ pypnc.py
@@ -13,26 +13,23 @@ def chyba():
     print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru"
     print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru.zip = nazov pod ktorym sa komprimovany subor ulozi. Pripona .zip bude pridana automaticky.\n"
 
-def chyba2(): #este do programu zakomponovat
-    print "Programu naozaj stacia 3 parametre...\n"
-print "Cakajte prosim, program robi co moze. Naozaj sa snazi."
-
-odkial=sys.argv[1]
-kam=sys.argv[2]
-meno=sys.argv[3]
-source=odkial
-cas=strftime("_%Y-%m-%d_%H:%M:%S", gmtime())
-subor=meno+cas+".zip"
-zip=zipfile.ZipFile(subor, 'w',zipfile.ZIP_DEFLATED) # nezipuje velke subory (treba zip64 extension)!
-rootlen=len(source)+1
-for base,dirs,files in os.walk(source):
-    for file in files:
-        fn=os.path.join(base,file)
-        zip.write(fn,fn[rootlen:])
-print "[OK]\nSubor sa nachadza v urcenom adresary",kam
 if len(sys.argv)<=3: #program vyzaduje 3 parametre
    chyba()
 else:
+    print "Cakajte prosim, program robi co moze. Naozaj sa snazi."
+    odkial=sys.argv[1]
+    kam=sys.argv[2]
+    meno=sys.argv[3]
+    source=odkial
+    cas=strftime("_%Y-%m-%d_%H:%M:%S", gmtime())
+    subor=meno+cas+".zip"
+    zip=zipfile.ZipFile(subor, 'w',zipfile.ZIP_DEFLATED) # nezipuje velke subory (treba zip64 extension)!
+    rootlen=len(source)+1
+    for base,dirs,files in os.walk(source):
+        for file in files:
+            fn=os.path.join(base,file)
+            zip.write(fn,fn[rootlen:])
+    print "[OK]\nSubor sa nachadza v urcenom adresary",kam
     with open('/home/peter/pwd','r') as infile:
         data=infile.read()
     my_list=data.splitlines()
@@ -42,5 +39,4 @@ else:
     ftp.prot_p()
     ftp.retrlines("LIST")
     ftp.storbinary("STORE"+subor, open(file, 'rb'))
-#    ftp.storlines('STOR'+subor, open(subor, 'rb'))
     ftp.close()
