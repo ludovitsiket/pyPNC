9b9790d9aa0ab70b867cf521bac58fc90eb67c7b
ludovitsiket <siket.ludovit@gmail.com>
2015-08-27 13:34:26 +0200

    hotova verzia aj s podporou ZIP64

--- pypnc.py
+++ pypnc.py
@@ -11,21 +11,21 @@ ps=[]
 my_list=[]
 souborek=""
 def chyba():
-    print "Nezadal si vsetky potrebne parametre programu."
+    print "Nezadali ste vsetky potrebne parametre programu."
     print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru"
     print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru.zip = nazov pod ktorym sa komprimovany subor ulozi. Pripona .zip bude pridana automaticky.\n"
 
 if len(sys.argv)<=3: #program vyzaduje 3 parametre
    chyba()
 else:
-    print "Cakajte prosim, program robi co moze." 
+    print "Cakajte prosim, program vytvara .zip subor. V zavislosti od velkosti to moze trvat niekolko minut." 
     odkial=sys.argv[1]
     kam=sys.argv[2]
     meno=sys.argv[3]
     source=odkial
     cas=strftime("_%Y-%m-%d_%H:%M:%S", gmtime())
     subor=meno+cas+".zip" #vysledne meno suboru
-    zip=zipfile.ZipFile(subor, 'w',zipfile.ZIP_DEFLATED) # nezipuje velke subory (treba zip64 extension)!
+    zip=zipfile.ZipFile(subor, 'w',zipfile.ZIP_DEFLATED,"allowZip64=True") # nezipuje velke subory (treba zip64 extension)!
     rootlen=len(source)+1
     for base,dirs,files in os.walk(source):
         for file in files:
@@ -41,7 +41,7 @@ else:
     path=my_list[2]
     server=my_list[3]
     for file in os.listdir('.'):
-        if fnmatch.fnmatch(file,"*.zip"):
+        if fnmatch.fnmatch(file,"*.iso"):
             fajl=str(file) #tymto tu si do fajl ulozim meno hotoveho suboru na odoslanie; pouzita kniznica fnmatch
     ftp=FTP_TLS(server,meno2,ps)   #pripojenie k serveru atd. 
     ftp.prot_p()
