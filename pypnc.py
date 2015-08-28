6cb0dfc2a5b17ec6f06dd14cc4fc71ed7135976a
ludovitsiket <siket.ludovit@gmail.com>
2015-08-28 12:10:53 +0200

    kontrola suboru, funkcia resend(),nejde timestamp

--- pypnc.py
+++ pypnc.py
@@ -6,14 +6,63 @@ import zipfile
 from time import gmtime, strftime
 from ftplib import FTP_TLS 
 import fnmatch
+
 meno=[]
 ps=[]
 my_list=[]
 file_to_zip=""
+cas=""
+#velkost_ftp="1234" #test
+velkost_ftp=""
+velkost_local=""
+reconnect=0
+max_recon=1 
+
+def nazov(timestamp_arg): #tato fcia nejde
+    if timestamp_arg == "time":
+        cas=strftime("_%Y-%m-%d_%H:%M:%S", gmtime()) 
+    elif timestamp_arg == "notime":
+        cas=""
+    else:
+        chyba()
+    return cas
+
 def chyba():
     print "Nezadali ste vsetky potrebne parametre programu."
-    print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru timestamp (time pre ulozenie casu do mena suboru, no pre vynechanie timestampu)"
+    print "Syntax: python pypnc.py cesta_k_zdroj_zlozke cesta_k_ciel_zlozke meno_suboru timestamp (time pre ulozenie casu do mena suboru, notime pre vynechanie timestampu)"
     print "cesta_k_zdroj_zlozke = cesta k zlozke ktora sa ma komprimovat \ncesta_k_ciel_zlozke = cesta k zlozke kam ma byt presunuty komprimovany subor \nmeno_suboru.zip = nazov pod ktorym sa komprimovany subor ulozi. Pripona .zip bude pridana automaticky.\n"
+    sys.exit()
+
+def connect(velkost_ftp):
+    ftp=FTP_TLS(server,meno2,ps)   
+    ftp.prot_p()
+    ftp.cwd(my_list[2]) 
+    ftp.retrlines("LIST")
+    print "Posielam subor. Cakajte prosim."
+    ftp.storbinary('STOR '+file_to_send, open(file_to_send, 'rb'),)
+    print "Subor odoslany [OK]"
+    size_ftp=ftp.nlst()
+    velkost_ftp_subor=size_ftp[0] #berie len prvy subor zo zoznamu
+    ftp.sendcmd("TYPE i")
+    velkost_ftp=ftp.size(velkost_ftp_subor) #test
+    ftp.close()
+    return velkost_ftp
+
+def resend(reconnect,max_recon):
+    print "Velkosti suborov sa lisia, pravdepodobne doslo k chybe.\nOpakujem posielanie suboru."
+    while reconnect <= max_recon :
+          reconnect=reconnect+1
+          connect(velkost_ftp)
+    print "Subor bol preposlany. Zistujem ci sa napravila chyba."
+    if velkost_local!=connect(velkost_ftp):
+       print "Chybu sa nepodarilo odstranit, skontrolujte subory manualne."
+
+def check_file_size(velkost_local):
+    if velkost_local!=connect(velkost_ftp):
+        resend(reconnect,max_recon)
+    else :
+        print "Meno a velkost preneseneho suboru sa zhoduju. [OK]"
+    sys.exit()
 
 if len(sys.argv)<=3: #program vyzaduje 3 parametre
    chyba()
@@ -22,21 +71,17 @@ else:
     odkial=sys.argv[1]
     kam=sys.argv[2]
     meno=sys.argv[3]
-    timestamp=sys.argv[4]
-    if timestamp == "time":
-        cas=strftime("_%Y-%m-%d_%H:%M:%S", gmtime()) 
-    elif timestamp =="no" : cas=""
-    else: None
+    timestamp_arg=sys.argv[4]
     source=odkial
+    nazov(timestamp_arg)
     subor=meno+cas+".zip" 
     zip=zipfile.ZipFile(subor, 'w',zipfile.ZIP_DEFLATED,"allowZip64=True") 
     rootlen=len(source)+1
     for base,dirs,files in os.walk(source):
         for file in files:
             fn=os.path.join(base,file)
-            file_to_zip=zip.write(fn,fn[rootlen:]) #file_to_zip je hotovy zazipovany subor
-            suborik=str(file_to_zip) #string premennej file_to_zip
-    print "[OK]\nSubor sa nachadza v urcenom adresary",kam
+            file_to_zip=zip.write(fn,fn[rootlen:]) 
+    print "Subor sa nachadza v urcenom adresary",kam,"[OK]"
     with open('/home/peter/pwd','r') as infile:
         data=infile.read()
     my_list=data.splitlines()
@@ -48,19 +93,5 @@ else:
         if fnmatch.fnmatch(file,"*.zip"):
             file_to_send=str(file) 
     velkost_local=os.path.getsize(file_to_send)
-    ftp=FTP_TLS(server,meno2,ps)   
-    ftp.prot_p()
-    ftp.cwd(my_list[2]) 
-    ftp.retrlines("LIST")
-    print "Posielam subor. Cakajte prosim."
-    ftp.storbinary('STOR '+file_to_send, open(file_to_send, 'rb'),)
-    print "[OK]"
-    size_ftp=ftp.nlst()
-    velkost_ftp_subor=size_ftp[0] #berie len prvy subor zo zoznamu
-    ftp.sendcmd("TYPE i")
-    velkost_ftp=ftp.size(velkost_ftp_subor)
-    ftp.close()
-    if velkost_local==velkost_ftp:
-       print "Meno a velkost preneseneho suboru sa zhoduju."
-    else: print "Velkosti suborov sa lisia, pravdepodobne doslo k chybe."
-    print "Spojenie ukoncene."
+    check_file_size(velkost_local)
+    connect(velkost_ftp)
